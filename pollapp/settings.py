eab1236287fe487c64b64df37db951cb65ec48f9
Fimber Uchenna Eze <uchenna631@gmail.com>
2022-08-06 12:59:48 +0000

    Add views for detail, results and vote

--- pollapp/settings.py
+++ pollapp/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'poll',
+    'poll.apps.PollConfig',
 ]
 
 MIDDLEWARE = [
@@ -116,7 +116,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'GB'
 
 USE_I18N = True
 

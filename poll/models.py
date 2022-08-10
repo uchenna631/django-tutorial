50c06ca3adc3d1f1c054ad2350e0456400b609db
Fimber Uchenna Eze <uchenna631@gmail.com>
2022-08-10 07:57:23 +0000

    Add fieldset and choice selection to the admin page

--- poll/models.py
+++ poll/models.py
@@ -6,7 +6,7 @@ import datetime
 
 class Question(models.Model):
     question_text = models.CharField(max_length = 200)
-    pub_date = models.DateTimeField('dadte published')
+    pub_date = models.DateTimeField('date published')
 
     def __str__(self):
         return self.question_text

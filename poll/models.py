918106b5a64c846a94a31e49e7ab31b4fda0586c
Fimber Uchenna Eze <uchenna631@gmail.com>
2022-08-10 08:10:33 +0000

    Customize admin page

--- poll/models.py
+++ poll/models.py
@@ -1,3 +1,4 @@
+from django.contrib import admin
 from django.db import models
 from django.utils import timezone
 import datetime
@@ -11,9 +12,14 @@ class Question(models.Model):
     def __str__(self):
         return self.question_text
 
+    @admin.display(
+        boolean=True,
+        ordering='pub_date',
+        description='Published recently?',
+    )
     def was_published_recently(self):
         now = timezone.now()
-        return now >= self.pub_date >= now - datetime.timedelta(days=1)
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
 
 class Choice(models.Model):
